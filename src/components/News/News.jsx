829260f00deb014a54bd9e51237dc8b86638ec99
Geo50 <gebrangeorgio@gmail.com>
2024-06-05 18:23:59 +0300

    Added loading SVG for news page + background image

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
 import s from "./News.module.css";
 import { Link } from "react-router-dom";
+import loadingSVG from "../../assets/pictures/SVG/bouncing-circles.svg";
+import GreyStripes from "../../assets/pictures/News/varying-stripes.png";
 
 const News = () => {
-  // const [randomNumber, setRandomNumber] = useState(
-  //   Math.floor(Math.random() * 31)
-  // );
-
   // eslint-disable-next-line no-unused-vars
+  const randomNumber = Math.floor(Math.random() * 31);
   const [news, setNews] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    document.body.style.backgroundImage = `url(${GreyStripes})`;
+    document.body.style.backgroundSize = "cover";
     const fetchData = async () => {
       try {
         const forexResponse = await fetch(
@@ -31,7 +32,7 @@ const News = () => {
         }
         const businessData = await businessResponse.json();
 
-        // Combine general and business news
+        // Combine forex and business news
         const combinedNews = [...forexData, ...businessData];
         setNews(combinedNews);
         setLoading(false);
@@ -42,10 +43,22 @@ const News = () => {
     };
 
     fetchData();
+    return () => {
+      document.body.style.backgroundImage = "";
+      document.body.style.backgroundSize = "";
+    };
   }, []);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className={s.loading}>
+        <img
+          src={loadingSVG}
+          alt="Loading Your News Articles..."
+          className={s.news_image}
+        />
+      </div>
+    );
   }
 
   if (error) {
@@ -56,6 +69,10 @@ const News = () => {
     return null;
   }
 
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+  };
+
   return (
     <>
       <h1>Financial News Across The Globe</h1>
@@ -63,7 +80,7 @@ const News = () => {
         <section className={s.wrapper}>
           {news
             .filter((item) => item.image) // Filter out items where image does not exist
-            .slice(0, 20)
+            .slice(randomNumber, randomNumber + 14)
             .map((item) => (
               <div key={item.id} className={s.news_container}>
                 <Link to={item.url} target="blank">
@@ -75,7 +92,9 @@ const News = () => {
                     />
                   )}
                   <p className={s.news_title}>{item.headline}</p>
-                  <p className={s.news_category}>Category: {item.category}</p>
+                  <p className={s.news_category}>
+                    Category: {capitalizeFirstLetter(item.category)}
+                  </p>
                 </Link>
               </div>
             ))}
